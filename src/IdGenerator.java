b41507f95146899e06453354e6ea9fc8e323c551
Andrey Didan <andrey_didan@mail.ru>
2024-03-20 15:25:10 +0300

    Перенёс класы в пакеты, учёл замечания

--- src/IdGenerator.java
+++
@@ -1,6 +0,0 @@
-public class IdGenerator {
-    private static int sequence = 0;
-    public static int generateNewId() {
-        return sequence++;
-    }
-}
--- src/StateTask.java
+++
@@ -1,5 +0,0 @@
-public enum StateTask {
-    NEW,
-    IN_PROGRESS,
-    DONE
-}
