955330f4331c1f87eb821a1afbc2a7b72aa67993
Andrey Didan <andrey_didan@mail.ru>
2024-04-08 20:43:01 +0300

    Исправлен конструктор Task
    Signed-off-by: Andrey Didan <andrey_didan@mail.ru>

--- test/task/manager/service/ManagersTest.java
+++ test/task/manager/service/ManagersTest.java
@@ -1,11 +1,6 @@
 package task.manager.service;
 
-import task.manager.model.Epic;
-import task.manager.model.SubTask;
-import task.manager.model.Task;
-
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 class ManagersTest {
